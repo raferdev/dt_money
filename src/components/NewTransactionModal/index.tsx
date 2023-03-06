90c475fed06242cbd5c58e77f067434daaafab75
Rafael <rafinha_fern@hotmail.com>
2023-03-06 00:25:35 -0300

    feat: new form buttons, debit and credit on new transaction modal

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,6 +1,6 @@
 import * as Dialog  from "@radix-ui/react-dialog";
-import { X } from "phosphor-react";
-import { CloseButton, Content, Overlay } from "./styles.js";
+import { ArrowCircleDown, ArrowCircleUp, X } from "phosphor-react";
+import { CloseButton, Content, Overlay, TransactionType, TransactionTypeButton } from "./styles.js";
 
 export function NewTransactionModal() {
     return(
@@ -16,7 +16,17 @@ export function NewTransactionModal() {
                 <input type='text' placeholder="Description" required/>
                 <input type='number' placeholder="Price" required/>
                 <input type='text' placeholder="Category" required/>
-            
+            <TransactionType>
+            <TransactionTypeButton variant="income">
+                <ArrowCircleUp size={24}/>
+                 Debit
+                </TransactionTypeButton>
+            <TransactionTypeButton variant="outcome">
+                <ArrowCircleDown size={24}/>
+                 Credit
+                </TransactionTypeButton>
+            </TransactionType>
+
             <button type="submit">
             Submit
             </button>
