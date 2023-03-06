92c0e2dca974457f34bc248aacd98a6d53c9e332
Rafael <rafinha_fern@hotmail.com>
2023-03-06 02:24:53 -0300

    refactor: move summary logic to useSummary hook.

--- src/components/Summary/index.tsx
+++ src/components/Summary/index.tsx
@@ -1,29 +1,10 @@
 import { SummaryCard, SummaryContainer } from "./styles.js";
 import {ArrowCircleDown, ArrowCircleUp, CurrencyDollar} from 'phosphor-react'
-import { useContext } from "react";
-import { TransactionContext } from "../../contexts/TransactionContext.js";
 import { priceFormatter } from "../../utils/formatter.js";
+import { useSummary } from "../../hooks/useSummary.js";
 
 export function Summary() {
-    const {transactions} = useContext(TransactionContext);
-   
-   const summary = transactions.reduce(
-    (acc,transaction)=>{
-        if(transaction.type==='income') {
-            acc.income+=transaction.price;
-            acc.total+=transaction.price;
-        } else {
-            acc.outcome+=transaction.price;
-            acc.total+=transaction.price;
-        }
-        return acc
-    }
-   ,{
-    income:0,
-    outcome:0,
-    total:0
-   })
-   
+    const summary = useSummary();
     return(
         <SummaryContainer>
             <SummaryCard>
---
+++ src/hooks/useSummary.ts
@@ -0,0 +1,24 @@
+import { useContext } from "react";
+import { TransactionContext } from "../contexts/TransactionContext.js";
+
+export function useSummary() {
+    const {transactions} = useContext(TransactionContext);
+   
+    const summary = transactions.reduce(
+     (acc,transaction)=>{
+         if(transaction.type==='income') {
+             acc.income+=transaction.price;
+             acc.total+=transaction.price;
+         } else {
+             acc.outcome+=transaction.price;
+             acc.total+=transaction.price;
+         }
+         return acc
+     }
+    ,{
+     income:0,
+     outcome:0,
+     total:0
+    })
+    return summary
+}
