fb89e8cde09d11fee5bd266bb4e8d21702c604e7
Rafael <rafinha_fern@hotmail.com>
2023-03-06 15:33:45 -0300

    fix: styles of search button.

--- src/pages/transactions/components/SearchForm/styles.ts
+++ src/pages/transactions/components/SearchForm/styles.ts
@@ -26,6 +26,7 @@ export const SearchFormContainer = styled.form`
     padding: 1rem;
     background: transparent;
     border: 1px solid ${(props) => props.theme['green-300']};
+    color: ${(props) => props.theme['green-300']};
     font-weight: bold;
     border-radius: 6px;
     cursor: pointer;
@@ -36,7 +37,7 @@ export const SearchFormContainer = styled.form`
     }
 
     &:not(:disabled):hover {
-      background: ${(props) => props.theme['gray-500']};
+      background: ${(props) => props.theme['green-500']};
       border-color: ${(props) => props.theme['green-500']};
       color: ${(props) => props.theme.white};
       transition: background-color 0.2s, color 0.2s, border-color 0.2s;
